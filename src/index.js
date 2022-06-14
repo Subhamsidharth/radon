22e6eed78580e232ff4a8b09bdf7a1890532f77e
Subhamsidharth <ssmahapatra1478@gmail.com>
2022-06-15 02:02:59 +0530

    middleware1sidh

--- src/index.js
+++ src/index.js
@@ -7,19 +7,35 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const midwareassign = function (req, res, next) {
+    var currentdate = new Date();
+    var datetime = "Last Sync: " + currentdate.getDate() + "/"
+        + (currentdate.getMonth() + 1) + "/"
+        + currentdate.getFullYear() + " @ "
+        + currentdate.getHours() + ":"
+        + currentdate.getMinutes() + ":"
+        + currentdate.getSeconds();
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+    let ip = req.ip
+    
+    let url = req.originalurl
+    console.log(`${datetime} ${ip} ${url}`)
+    next()
+}
+app.use(midwareassign)
+
+mongoose.connect("mongodb://localhost:27017", {
     useNewUrlParser: true
 })
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
 
-app.use (
+app.use(
     function (req, res, next) {
-        console.log ("inside GLOBAL MW");
+        console.log("inside GLOBAL MW");
         next();
-  }
-  );
+    }
+);
 
 app.use('/', route);
 
